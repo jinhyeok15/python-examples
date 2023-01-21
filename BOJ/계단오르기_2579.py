97f683455e773a377a40906b0cbe82668fb9b4bb
jinhyeok15 <ekfrhd97@gmail.com>
2023-01-22 01:55:54 +0900

    계단오르기_2579

--- BOJ/계단오르기_2579.py
+++ BOJ/계단오르기_2579.py
@@ -1,68 +1,16 @@
 N = int(input())
-stairs = [int(input()) for _ in range(N)]
+stairs = []
+for _ in range(N):
+    stairs.append(int(input()))
 
+m = []
 
-class LookUp:
-    def __init__(self,
-        current: int,
-        score: int,
-        accum: int,
-        strike: int
-    ):
-        self.current = current
-        self.score = score
-        self.accum = accum
-        self.strike = strike
-
-
-def bfs(start, stairs):
-    q = []
-    q.append(LookUp(start, stairs[start], stairs[start], 1))
-
-    max_score = 0
-
-    while q:
-        lookup: LookUp = q.pop(0)
-
-        if lookup.current == len(stairs)-1:
-            max_score = max([lookup.accum, max_score])
-            continue
-
-        plans = next_plans(lookup, stairs)
-        for plan in plans:
-            q.append(plan)
-    
-    return max_score
-
-
-def next_plans(lookup: LookUp, stairs):
-    plans = []
-
-    top = len(stairs)-1
-    if lookup.current == top:
-        return plans
-
-    if lookup.strike + 1 != 3:
-        idx = lookup.current + 1
-        plans.append(
-            LookUp(idx, stairs[idx], lookup.accum + stairs[idx], lookup.strike + 1)
-        )
-    
-    if lookup.current + 2 > top:
-        return plans
-
-    idx = lookup.current + 2
-    plans.append(
-        LookUp(idx, stairs[idx], lookup.accum + stairs[idx], 1)
-    )
-
-    return plans
-
-
-if N == 1:
-    print(bfs(0, stairs))
+if N <= 2:
+    print(sum(stairs))
 else:
-    print(max([bfs(0, stairs), bfs(1, stairs)]))
-
-# N = 6
-# stairs = [10, 20, 15, 25, 10, 20]
+    m.append(stairs[0])
+    m.append(stairs[0]+stairs[1])
+    m.append(max(stairs[1]+stairs[2], stairs[0]+stairs[2]))
+    for i in range(3, N):
+        m.append(max(m[i-3]+stairs[i-1]+stairs[i], m[i-2]+stairs[i]))
+    print(m[-1])
