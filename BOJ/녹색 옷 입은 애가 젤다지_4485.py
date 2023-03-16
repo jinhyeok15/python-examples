9ddfcf8cf32f105d266f9f6f62525d641891c0c5
jinhyeok15 <ekfrhd97@gmail.com>
2023-03-16 18:24:27 +0900

    녹색 옷을 입은 애가 젤다지 재풀이

--- BOJ/녹색 옷 입은 애가 젤다지_4485.py
+++ BOJ/녹색 옷 입은 애가 젤다지_4485.py
@@ -21,28 +21,29 @@ cnt = 1
 while True:
     N = int(input())
     if N == 0: break
-    cave = []
-    for i in range(N):
-        cave.append(list(map(int, input().split())))
-    
-    min_plan = [[float('inf') for _ in range(N)] for _ in range(N)]
-    start = (0, 0)
-    min_plan[start[0]][start[1]] = cave[start[0]][start[1]]
 
-    queue = [(cave[start[0]][start[1]], start)]
+    graph = {}
+    for i in range(N):
+        for j, v in enumerate(list(map(int, input().split()))):
+            graph[(i, j)] = [v, float('inf')]
+
+    start = (0, 0)
+    graph[start][1] = graph[start][0]
+
+    queue = [(graph[start][0], start)]
     while queue:
         v, curr = heapq.heappop(queue)
 
-        if min_plan[curr[0]][curr[1]] < v:
+        if graph[curr][1] < v:
             continue
 
         for path in go(curr, N):
             y, x = path
-            acc = v+cave[y][x]
-
-            if acc < min_plan[y][x]:
-                min_plan[y][x] = acc
+            
+            acc = v+graph[path][0]
+            if acc < graph[path][1]:
+                graph[path][1] = acc
                 heapq.heappush(queue, (acc, path))
 
-    print(f"Problem {cnt}: {min_plan[N-1][N-1]}")
+    print(f"Problem {cnt}: {graph[(N-1, N-1)][1]}")
     cnt += 1
