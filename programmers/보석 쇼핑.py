d2369eed0fde63996f068915c2322eb6fa1bc834
jinhyeok15 <ekfrhd97@gmail.com>
2023-03-19 15:03:04 +0900

    보석쇼핑

--- programmers/보석 쇼핑.py
+++ programmers/보석 쇼핑.py
@@ -41,3 +41,45 @@ def preprocess_heap_data(q):
         q.appendleft(comp)
         return (len(q), q)
     q.appendleft(comp); q.appendleft(item); return (len(q), q)
+
+
+# ----------------------------------------------------------------------
+import heapq
+
+
+def solution(gems):
+    answer = [1, 1]
+
+    counter = {}
+    for gem in enumerate(gems):
+        if gem not in counter:
+            counter[gem] = 0
+    
+    visited = set()
+    category = set(gems)
+    heap = []
+    
+    for i, gem in enumerate(gems):
+        start, end = answer
+        if i == 0: continue
+        
+        if gems[start-1] == gem:
+            start += 1
+            end = i
+        else:
+            end = i
+            visited.add(gem)
+            counter[gem] += 1
+
+        if visited == category:
+            heapq.heappush(heap, (end-start, [start, end]))
+            cnt = counter[gems[start-1]]
+            cnt -= 1
+            if cnt == 0:
+                visited.remove(gems[start-1])
+            counter[gems[start-1]] = cnt
+            answer = [start+1, end]
+        else:
+            answer = [start, end]
+    
+    return answer
